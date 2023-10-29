be825efaafbc7b792ad0ab204adcf722c9f3dc1e
Galbrush <cathrin.steiner@hotmail.de>
2023-10-29 21:12:46 +0100

    add description of tester process

---
+++ tester.py
@@ -0,0 +1,10 @@
+from buldRenamer import *
+
+# 1. Rename files and log what happened
+# 2. Delete the files
+# 3. Create new files with "test" as name
+
+#C:\Users\cathr\OneDrive\42\0. Projects\python_BuldRenamer\PythonFileRenamer\TestDocuments
+
+
+file_renamer("C:\\Users\\cathr\\OneDrive\\42\\0. Projects\python_BuldRenamer\\PythonFileRenamer\\TestDocuments", "mynewname")
