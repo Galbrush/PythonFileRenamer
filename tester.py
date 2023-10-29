108fdc026c8c3883b0bcffa7e4c4f33f02af9180
Galbrush <cathrin.steiner@hotmail.de>
2023-10-29 21:40:25 +0100

    add test functionality to tester file

--- tester.py
+++ tester.py
@@ -1,10 +1,22 @@
 from buldRenamer import *
 
+path = "C:\\Users\\cathr\\OneDrive\\42\\0. Projects\\python_BuldRenamer\\PythonFileRenamer\\TestDocuments"
+testName = "myNewName" + str(counter)
+def create_new_file(path, file):
+    with open(os.path.join(path, file), 'w') as fp: 
+        pass
+
+
 # 1. Rename files and log what happened
 # 2. Delete the files
 # 3. Create new files with "test" as name
 
-#C:\Users\cathr\OneDrive\42\0. Projects\python_BuldRenamer\PythonFileRenamer\TestDocuments
+file_renamer(path, testName)
+
+create_new_file(path, "test.txt")
+create_new_file(path, "test.docx")
+create_new_file(path, "test.xlsx")
+
+print("Test is Completed")
 
 
-file_renamer("C:\\Users\\cathr\\OneDrive\\42\\0. Projects\python_BuldRenamer\\PythonFileRenamer\\TestDocuments", "mynewname")
