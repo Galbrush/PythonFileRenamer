2d966040e03af606338eb5344572f4f5d9edcf73
Galbrush <cathrin.steiner@hotmail.de>
2023-10-29 23:09:03 +0100

    add possibility to only change specific file types

--- buldRenamer.py
+++ buldRenamer.py
@@ -1,20 +1,32 @@
 
 import os
 
-def file_renamer(directory, newName):
+def file_renamer(directory, newName, fileExtension = None):
 
     files = os.listdir(directory)
     global counter
     counter = 0
 
+
     for file in files:
-        try:
-            fileType = file.split('.')[-1] 
-            os.rename(directory + '/' + file, directory + '/' + newName + str(counter) + '.' + fileType)
-            print("Renaming " + file + " to " + newName + str(counter) + "." + fileType)
-            counter += 1
-        except: 
-            print("Couldn't rename file")
+        if fileExtension == None:
+            try:
+                fileType = file.split('.')[-1] 
+                os.rename(directory + '/' + file, directory + '/' + newName + str(counter) + '.' + fileType)
+                print("Renaming " + file + " to " + newName + str(counter) + "." + fileType)
+                counter += 1
+            except: 
+                print("Couldn't rename file")
+        else: 
+
+            try:
+                if file.endswith(fileExtension):
+                    fileType = file.split('.')[-1] 
+                    os.rename(directory + '/' + file, directory + '/' + newName + str(counter) + '.' + fileType)
+                    print("Renaming " + file + " to " + newName + str(counter) + "." + fileType)
+                    counter += 1
+            except: 
+                print("Couldn't rename file")
 
 
     
