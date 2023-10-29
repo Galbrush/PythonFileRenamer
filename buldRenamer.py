0b08af1674390d65e7d4ebd1feb523e48226bd34
Galbrush <cathrin.steiner@hotmail.de>
2023-10-29 21:04:27 +0100

    add file_renamer function

--- buldRenamer.py
+++ buldRenamer.py
@@ -1,1 +1,17 @@
 
+import os
+
+def file_renamer(directory, newName):
+
+    files = os.listdir(directory)
+    counter = 0
+
+    for file in files:
+        fileType = file.split('.')[-1] 
+        os.rename(directory + '/' + file, directory + '/' + newName + str(counter) + '.' + fileType)
+        print("Renaming " + file + " to " + newName + str(counter) + "." + fileType)
+        counter += 1
+    
+    
+
+    
